29202c59bc3d2c963ceea8ccdb9927538309c1a1
Vimal Saini <vimal.saini25@gmail.com>
2025-03-20 14:29:25 +0530

    Removed the debugging code

--- backend/app/routes/v1/health.py
+++ backend/app/routes/v1/health.py
@@ -1,19 +1,14 @@
-from flask import Blueprint, current_app, jsonify
+from flask import Blueprint, jsonify
 
-from app.tasks import export_orders
-from app.mail import get_outlook_token, send_login_otp
 
 router = Blueprint("health", __name__)
 
 @router.route('/health-check', methods=['GET'])
 def check():
-    access_token = get_outlook_token()
-    send_login_otp(access_token, "veesysdevel@gmail.com", {"otp": "121112", "name": "John Doe","app_name": current_app.config.get("APP_NAME")})
     response = {
         "code": 200,
         'message': 'Working!',
         "data": None,
         "errors": {},
     }
-    #export_orders.delay(3)
     return jsonify(response), response["code"]
