9ff7bad61d2d9979dcf772bf819e1674c29387ba
Vimal Saini <vimal.saini25@gmail.com>
2025-03-21 17:37:44 +0530

    Updated docker compose

--- backend/app/routes/v1/health.py
+++ backend/app/routes/v1/health.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
+from app.tasks import export_orders
 
 
 router = Blueprint("health", __name__)
@@ -11,4 +13,7 @@ def check():
         "data": None,
         "errors": {},
     }
+    _export = request.args.get('_export')
+    if _export:
+        export_orders.delay(_export)
     return jsonify(response), response["code"]
