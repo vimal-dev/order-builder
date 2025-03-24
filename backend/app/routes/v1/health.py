cebb95ede9c9ed962d5b4b728e484ac9f9017c38
Vimal Saini <vimal.saini25@gmail.com>
2025-03-25 00:32:32 +0530

    Removed the debugging code

--- backend/app/routes/v1/health.py
+++ backend/app/routes/v1/health.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, current_app, jsonify, request
+from flask import Blueprint, jsonify, request
 
-from app.mail import get_outlook_token, send_order_received
 from app.tasks import export_orders
 
 
@@ -8,16 +7,6 @@ router = Blueprint("health", __name__)
 
 @router.route('/health-check', methods=['GET'])
 def check():
-    # data = {
-    #     "app_name": current_app.config.get("APP_NAME"),
-    #     "customer_name": "veesysdevel@gmail.com",
-    # }
-    # try:
-    #     access_token = get_outlook_token()
-    #     send_order_received(access_token, "veesysdevel@gmail.com", data)
-    #     # result.append(order.get("id"))
-    # except Exception as e:
-    #     current_app.logger.error(str(e))
     response = {
         "code": 200,
         'message': 'Working!',
